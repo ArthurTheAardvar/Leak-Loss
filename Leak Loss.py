3346211edcafb323f1229a825f8aaf581f263057
ArthurTheAardvar <114256031+ArthurTheAardvar@users.noreply.github.com>
2024-02-17 10:32:26 -0700

    Update Leak Loss.py

--- Leak Loss.py
+++ Leak Loss.py
@@ -1,5 +1,5 @@
 import sys #needed for readline function
-import math
+
 cases = int(sys.stdin.readline().rstrip()) #get the first number of cases
 
 for i in range(cases): #go through the other lines
@@ -8,9 +8,9 @@ for i in range(cases): #go through the other lines
     line = line.split(" ") #split up the line by spaces
     
 
-    Total = (int(line[0]) / (int(line[1]) - int(line[2]))) * int(line[2])
+    Total = (float(line[0]) / (float(line[1]) - float(line[2]))) * float(line[2])
 
-    print(int(Total))
+    print(float(Total))
     
     
     
