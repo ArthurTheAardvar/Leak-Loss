fd7210a69931d125b51d3bb84efbfc87d0f5c776
ArthurTheAardvar <114256031+ArthurTheAardvar@users.noreply.github.com>
2024-02-22 14:24:20 -0700

    Update Leak Loss.py

--- Leak Loss.py
+++ Leak Loss.py
@@ -1,9 +1,10 @@
+
 cases = int(input()) 
 
 for i in range(cases): 
     
-    line = input.rstrip()
-    line = line().split(" ") 
+    line = input().rstrip()
+    line = line.split(" ") 
     
     Total = (float(line[0]) / (float(line[1]) - float(line[2]))) * float(line[2])
 
